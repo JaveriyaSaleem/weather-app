4b9fe415eccba66fb229ff3ad458ec90dbc99219
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-01-09 12:56:52 +0500

    added

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useRef } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  let inputRef = useRef()
+  const apiKey = '0042bc4f7f30e066c405fba7b88a57fa'
+const getCity = async()=>{
+  let cityStore = inputRef.current.value
+  console.log(inputRef.current.value)
+try{
+let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityStore}&appid=${apiKey}&units=metric`)
+console.log(response)
+console.log("temperature: ", response.data.main.temp)
+console.log("Feels Like: ", response.data.main.feels_like)
+console.log("City Name: ", response.data.name)
+console.log("Country: ",response.data.sys.country)
+console.log("timezone: ",response.data.timezone)
+console.log("Description: ",response.data.weather[0].description)
+const utcTime = new Date().getTime()
+const localTime = new Date(utcTime + response.data.timezone * 1000); 
+      console.log("Local Time: ", localTime.toLocaleString()); 
+
+}catch(e){
+  console.log(e)
+}
+  
+  inputRef.current.value = ""
+}
+  
+  return (
+    <div>
+      
+      <input type="text" ref={inputRef}/>
+      <button onClick={getCity}>Search</button>
+
+    </div>
+  )
+}
+
+export default App
